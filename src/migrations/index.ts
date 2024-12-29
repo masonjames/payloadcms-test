74446cf139f94ba03eb65792134fd455e28d6e70
Mason James <mason@growagainorchids.com>
2024-12-28 22:21:20 -0500

    less migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20241221_130412_initial from './20241221_130412_initial';
-import * as migration_20241229_012725 from './20241229_012725';
+import * as migration_20241229_012725 from './20241229_012725'
 
 export const migrations = [
   {
-    up: migration_20241221_130412_initial.up,
-    down: migration_20241221_130412_initial.down,
-    name: '20241221_130412_initial',
-  },
-  {
     up: migration_20241229_012725.up,
     down: migration_20241229_012725.down,
-    name: '20241229_012725'
+    name: '20241229_012725',
   },
-];
+]
